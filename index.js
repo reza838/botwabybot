f6dabbb6e37233f8a41b31a28d0ac291bc350e11
SH 鑫 <70884565+bittier@users.noreply.github.com>
2021-09-26 00:01:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Loading To Mastah\nHL Bot', {
+CFonts.say('Loading To Mastah\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
